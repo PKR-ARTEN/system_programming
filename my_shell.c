64815db84208621b89e1bec2622cb07c125c9922
pkrarten <qkw1207@naver.com>
2019-04-06 07:40:51 +0900

    add ls

--- my_shell.c
+++ my_shell.c
@@ -3,29 +3,35 @@
 #include <stdlib.h>
 
 void print_prompt();
+void m_ls();
 
 int main(){
 
 	char *e = "exit";
+	char *ls = "ls";
+	char *hello = "hello";
 	char *command;
 	char *command_c;
 
 	system("clear");
-	
-	print_prompt();
 
 	while(1){
+
+		print_prompt();
 		
 		scanf("%s", command);
 		command_c = strdup(command);
 
 		if(strcmp(command_c, e)==0){
-			printf("shell close\n");
 			system("clear");
 			exit(0);
+		} else if(strcmp(command_c, ls)==0){
+			printf("show direcory\n");
+			m_ls(NULL);
+		} else if(strcmp(command_c, hello)==0){
+			printf("hello!\n");
 		} else {
 			printf("no command\n");
-			print_prompt();
 		}
 	}
 	
